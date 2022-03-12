0748cb16ecb82ecb21ecce43cd79df1d47d0ad4e
AzizAmbivert <azizwekesa2001@gmail.com>
2022-03-12 00:40:50 -0800

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-
-/* 
- * puts function 
- *
- */
 #include <stdio.h>
-#include<string.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-char str1[70];
-strcpy(str1, "Programming is like building a maltilingual puzzle\n");
-puts(str1);
-return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
-
