eb995490a84fc78f43db615dfe8ec5f64c67a572
AzizAmbivert <azizwekesa2001@gmail.com>
2022-03-18 08:29:32 -0700

    Numbers have life theyre not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,27 @@
-nclude "holberton.h"
+#include "main.h"
 
 /**
- *  * print_number - print an integer (with no trailing newline)
- *   * @n: integer to print
- *    *
- *     * Return: void
- *      */
+ *print_number - print an integer (with no trailing newline)
+ *@n: integer to print
+ *Return: void
+ */
 void print_number(int n)
 {
-		unsigned int m;
+	unsigned int m;
 
-			if (n >= 0)
-					{
-								if (n >= 10)
-												print_number(n / 10);
-										_putchar('0' + n % 10);
-											}
-				else
-						{
-									_putchar('-');
-											m = -1 * n;
-													if (m >= 10)
-																	print_number(m / 10);
-															_putchar('0' + m % 10);
-																}
+	if (n >= 0)
+	{
+		if (n >= 10)
+			print_number(n / 10);
+			_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('-');
+		m = -1 * n;
+	if (m >= 10)
+		print_number(m / 10);
+		_putchar('0' + m % 10);
+	}
 
 }
